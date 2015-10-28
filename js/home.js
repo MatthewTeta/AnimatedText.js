247959700c9e3d0177ab8d55fe43a89350bafde1
MatthewTeta <randommagicmatt@gmail.com>
2015-10-28 10:29:37 -0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 
 function animatePage() {
     var headerText = $('.animate#header').html();
-    var headerAnimateInterval = 50;
+    var headerAnimateInterval = 10;
     var subText = $('.animate#sub-text').html();
-    var subTextAnimateInterval = 50;
+    var subTextAnimateInterval = 10;
     
     $('div.centered').show();
     
